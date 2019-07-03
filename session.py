a3933d5a43f7752dd6c3c80811212ec0c4e14935
park seong kyu <44438709+ndymion3217@users.noreply.github.com>
2019-07-03 21:39:41 +0900

    Update session.py

--- session.py
+++ session.py
@@ -1,13 +1,45 @@
 ﻿#import random as rd
+import resource
+from item import Item
+from monster import *
 
 class Session:
     pass
 
 class Village:
-    pass
+    def __init__(self, name):
+        if name == "세이룬":
+            self.inn = [Store("부드러운 검", "싸구려 여관")]
+            script = NpcSctipt('안녕하시오', "감사하오", "나는 이 '부드러운 검' 여관을 20년간 지켜온 랄프라 하오", "안녕히 가시오", "퀘스트를 줍니다.")
+            test = Npc("랄프", 41, "남", script)
+            self.inn[0].set_npc(test)
+            self.is_player_here = False
 
-class Map:
-    pass
+    def show_info(self):
+        print("두리번 거려보니 저기에 35년 전통을 자랑하는 '부드러운 검' 여관이 있는것이 보이는군요.")
+
+
+class Spot:
+    def __init__(self):
+        pass
+
+    def show_info(self):
+        pass
+
+
+class Dungeon:
+    def __init__(self):
+        pass
+
+
+class TrainingCnter(Dungeon):
+    def __init__(self):
+        self.name = "연습투기장"
+        self.visit_count = 0
+        self.first_wave = [Monster('고블린'), Monster('고블린')]
+        self.second_wave = None
+        self.quest = None
+
 
 class Npc:
     def __init__(self, name, age, gender, script, store=None):  # NPC캐릭터의 이름, 나이, 대화내용들을 준비합니다.
@@ -29,11 +61,11 @@ class Npc:
     def say_thankyou(self): # 감사하다고 출력합니다.
         print(self.script.thankyou)
 
-    def open_store(self):  # 해당 npc가 가지고있는 상품을 보여줍니다.
+    def open_store(self, character):  # 해당 npc가 가지고있는 상품을 보여줍니다.
         if self.store == None:
             print("소유하고 있는 상점이 없습니다.")
         else:
-            self.store.show_menu()
+            self.store.show_menu(character)
 
     def say_goodbye(self):  # 헤어질때 인사를 출력합니다.
         print(self.script.bye)
@@ -47,13 +79,50 @@ class Store:
         self.name = name
         self.type = store_type
         self.npc = npc
+        self.service = Service(store_type)
+        self.selling = Selling(store_type)
 
-    def show_menu(self):
-        print("가게 메뉴를 보여줍니다.")
+    def show_menu(self, character):
+        self.service.show_menu(character)
+        self.selling.show_menu(character)
 
     def set_npc(self, npc):
         self.npc = npc
         self.npc.set_store(self)
+
+    def sell_stuff(self, name, character):
+        for item in resource.item_list:
+            if name == item[0]:
+                if character.inventory.money > item[2]:
+                    pass
+
+
+    def get_service(self, name):
+        pass
+
+
+class Service:
+    def __init__(self, type):
+        if type == '싸구려 여관':
+            self.sleep = resource.cheap_inn_sleep
+            self.meals = resource.cheap_inn_meals
+
+    def show_menu(self, character):
+        print('--------서비스--------')
+        print(self.sleep[0], self.sleep[1] - character.status.cha, "닢")
+        print(self.meals[0], self.meals[1], '닢')
+
+class Selling:
+    def __init__(self, type):
+        self.slot = []
+        if type == "싸구려 여관":
+            for item in resource.item_list:
+                self.slot.append(Item(item[0]))
+
+    def show_menu(self, character):
+        print('---------판매--------')
+        for item in self.slot:
+            print('{} {}회분 {}닢 무게 {}'.format(item.name, item.pcs, item.price, item.weight))
 
 
 class Field:
@@ -69,6 +138,7 @@ class NpcSctipt:
         self.bye = bye
 
 
+<<<<<<< HEAD
 class Dungeon:
     def __init__(self):
         self.name = name
@@ -90,3 +160,5 @@ def test_code():
     test_store.set_npc(test)
     test.open_store()
 test_code()
+=======
+>>>>>>> f4be0723840cbba400f531ced31d6880da09dedb
